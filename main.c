c77c9de8a89a48e9a0d136f3f221c3f3ca769b1b
Leonardo de Souza Galvão <160603231+LeonardodeSouzaGalvao@users.noreply.github.com>
2024-04-08 08:29:27 -0300

    Verde Q19

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//Crie um programa que remova todos os espaços em branco de uma string.tamanho máximo 200 cada string .
 int main()
 {
- int a,b;
- printf("valor de a = ");
- scanf("%d", & a);
- printf("valor de b = ")
- scanf("%d", & b);
- a+b
+    char frase[201], frase2[201];
+    int i, j=0;
+    scanf("%[^\n]", &frase, &frase2);
 
+    for(i=0; frase[i] != '\0';i++)
+    {
+        if( frase[i] == ' ')
+        {
+            continue;
+        }
 
+        else
+        {
+            frase2[j++] = frase[i];
 
+        }
+    }
+    printf("string:%s",frase2);
+    return 0;
 }
-
